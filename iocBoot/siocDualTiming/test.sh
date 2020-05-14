e60f6e626a9a8447c3eb289f70efdea617697547
Eric Norum <wenorum@lbl.gov>
2020-05-14 13:39:44 -0700

    Start with sequence debug off for now.  Shorten script.

--- iocBoot/siocDualTiming/test.sh
+++ iocBoot/siocDualTiming/test.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
 # Start soft IOC in test mode
-EVG_ADDRESS="${EVG_ADDRESS=192.168.1.129}"
+export EVG_ADDRESS="${EVG_ADDRESS=192.168.1.129}"
 case "$#" in
     1)  EVG_ADDRESS="$1" ;;
     *)
 esac
-export EVG_ADDRESS
 export FPGA_SIMM_DISABLE=""
-export SEQ_DEBUG="1"
+export SEQ_DEBUG="0"
 export P="testEVG"
 export R=":"
 export T="test"
