3cc0e0db31b3642a3bb95df35fd86b0e7734e911
Eric Norum <wenorum@lbl.gov>
2020-11-30 20:20:31 -0800

    Add help for tracking operation.

--- iocBoot/siocDualEventGenerator/run.sh
+++ iocBoot/siocDualEventGenerator/run.sh
@@ -7,14 +7,15 @@ export TRACK_OLD_EVG_DISABLE="#"
 for i in "$@"
 do
     case "$i" in
-        [0-9][0-9]*.[0-9][0-9]*.[0-9][0-9]*.[0-9][0-9]*.)  EVG_ADDRESS="$1" ;;
+        [0-9]*)  EVG_ADDRESS="$1" ;;
         -s) FPGA_SIMM_DISABLE="" ;;
         -t) TRACK_OLD_EVG_DISABLE="" ;;
         -a) export AUTOSAVE_PATH="\$(TOP)/autosave" ;;
-        -*) echo "Usage: $0 [-a] [-h] [-s]" >&2
+        -*) echo "Usage: $0 [-a] [-h] [-s] [-t] [EVG_ADDRESS]" >&2
             echo "       -a -- Use local autosave/restore directory" >&2
             echo "       -h -- Show this help messdage, then exit" >&2
             echo "       -s -- Place EVG records into simulation mode" >&2
+            echo "       -t -- Track old EVG" >&2
             exit 1 ;;
     esac
 done
