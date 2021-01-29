74ec9ad09675aa196a86d9e3114f7bf275b858a2
Eric Norum <wenorum@lbl.gov>
2021-01-29 14:13:06 -0800

    Add explicit check on timestamps.  No more 'long enough' wait.

--- scripts/Latencies.py
+++ scripts/Latencies.py
@@ -12,14 +12,14 @@ import time
 
 parser = argparse.ArgumentParser(description='Measure latencies on all channels.', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('-p', '--prefix', default='EVG:', help='Record name prefix')
-parser.add_argument('-e', '--external', action='store_true', help='Subtract local FPGA to crosspoint switch latency')
+parser.add_argument('-e', '--external', action='store_true', help='Measure external latency by subtracting local FPGA to crosspoint switch latency')
 args = parser.parse_args()
 
 class EVG:
     def __init__(self, prefix, evg):
         self.internalLatency = 0.0
-        self.latency = (epics.PV(args.prefix + 'E%d:latency' % evg))
-        self.loopback = (epics.PV(args.prefix + 'E%d:loopback' % evg))
+        self.latency = epics.PV(args.prefix + 'E%d:latency' % evg,form='time')
+        self.loopback = epics.PV(args.prefix + 'E%d:loopback' % evg,form='time')
 
     def setInternalLatency(self, newInternalLatency=None):
         if newInternalLatency == None:
@@ -31,7 +31,11 @@ class EVG:
         if ((chan < 0) or (chan > 36)):
             raise ValueError("Channel out of range")
         self.loopback.put(chan, wait=True)
-        time.sleep(0.02)
+        self.loopback.get_timevars()
+        while True:
+            self.latency.get_timevars()
+            if self.latency.timestamp >= self.loopback.timestamp: break
+            time.sleep(0.001)
         return self.latency.get() - self.internalLatency
 
 evgs = []
