b9cd084f3d512a505f5476af44f3888e017e161c
Eric Norum <wenorum@lbl.gov>
2021-02-25 17:52:55 -0800

    Use better default sequences.  Use swapout interval that can be used for testing BCM in lab.

--- scripts/SequencerTest.py
+++ scripts/SequencerTest.py
@@ -72,8 +72,8 @@ parser.add_argument('-c', '--cycles', type=int, default=0, help='Number of "sequ
 parser.add_argument('-e', '--evg', type=int, default=1, choices=(1,2), help='Event generator to use')
 parser.add_argument('-p', '--prefix', default='EVG:', help='Record name prefix')
 parser.add_argument('-s', '--swapout', type=int, default=0, help='Swapout trigger offset')
-parser.add_argument('-0', '--seq0', default='0,10,1826573,12,3,18,0,20,1,24,0,26,0,28,57762643,39,641919,126,3,38,8067,50,2,56,2499991,70,0,127', help='Sequence 0')
-parser.add_argument('-1', '--seq1', default='0,10,1826573,12,3,18,0,20,1,24,0,26,0,28,57762643,39,641919,126,3,38,8067,50,2,56,2499941,68,40,70,0,127', help='Sequence 1')
+parser.add_argument('-0', '--seq0', default='0,10,1826573,12,3,18,0,20,1,24,0,26,0,28,57762643,39,3,38,8067,50,2,56,100,70,0,127', help='Sequence 0')
+parser.add_argument('-1', '--seq1', default='0,10,1826573,12,3,18,0,20,1,24,0,26,0,28,57762643,39,641919,126,3,38,8067,50,2,56,100,68,40,70,0,127', help='Sequence 1')
 args = parser.parse_args()
 
 pattern0 = parseSequence(args.seq0)
@@ -108,7 +108,7 @@ if args.cycles > 0:
                 print("Missed sequence!", file=sys.stderr)
                 seq0Count = seqCount[0]
         else:
-            time.sleep(1.0)
+            time.sleep(0.6)
             swapoutTrigger.put(args.swapout, wait=True)
             awaitSequenceCompletion()
         args.cycles -= 1
