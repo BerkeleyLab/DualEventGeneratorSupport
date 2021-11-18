d1323fc5e8da9c7e562b9c161944c82f6971ccdb
Eric Norum <wenorum@lbl.gov>
2021-11-18 14:24:48 -0800

    Restore loopback when complete.

--- scripts/Latencies.py
+++ scripts/Latencies.py
@@ -2,6 +2,7 @@
 
 #
 # Measure latencies on all channels
+# FIXME: Needs some mechanism for specifying presence of fanout modules
 #
 
 from __future__ import print_function
@@ -12,18 +13,18 @@ import time
 
 parser = argparse.ArgumentParser(description='Measure latencies on all channels.', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('-p', '--prefix', default='EVG:', help='Record name prefix')
-parser.add_argument('-e', '--external', action='store_true', help='Measure external latency by subtracting local FPGA to crosspoint switch latency')
 parser.add_argument('-v', '--verbose', action='store_true', help='Enable some additional diagnostic messages.')
 args = parser.parse_args()
 
 class EVG:
     def __init__(self, prefix, evg):
-        self.localLatency = -1.0
+        self.initialLoopback = -1
         self.latency = epics.PV(args.prefix + 'E%d:latency'%(evg),form='time',auto_monitor=True)
         self.loopback = epics.PV(args.prefix + 'E%d:loopback'%(evg),form='time',auto_monitor=True)
         self.loopbackPROC = epics.PV(args.prefix + 'E%d:loopback.PROC'%(evg))
+        self.initialLoopback = self.loopback.get();
 
-    def getAbsoluteLatencyForChannel(self, chan):
+    def getLatencyForChannel(self, chan):
         if ((chan < 0) or (chan > 36)):
             raise ValueError("Channel out of range")
         self.loopback.put(chan, wait=True)
@@ -46,14 +47,10 @@ class EVG:
         l = self.latency.get()
         return l
 
-    def getRelativeLatencyForChannel(self, chan):
-        # Lazy initialization of local latency measurement
-        if self.localLatency < 0:
-            self.localLatency = self.getAbsoluteLatencyForChannel(36)
-        l = self.getAbsoluteLatencyForChannel(chan)
-        # Local loopback measurement is always absolute
-        if chan < 36: l -= self.localLatency
-        return l
+    def restore(self):
+        if self.initialLoopback >= 0:
+            self.loopback.put(self.initialLoopback, wait=True)
+        self.initialLoopback = -1
 
 evgs = []
 for e in (1, 2):
@@ -61,9 +58,8 @@ for e in (1, 2):
 for channel in range(1, 37):
     print('%2d:' % (channel), end='')
     for evg in evgs:
-        if args.external:
-            l = evg.getRelativeLatencyForChannel(channel)
-        else:
-            l = evg.getAbsoluteLatencyForChannel(channel)
+        l = evg.getLatencyForChannel(channel)
         print('%7.1f' % (l), end='')
     print('')
+for evg in evgs:
+    evg.restore()
